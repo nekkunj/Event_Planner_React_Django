e40ab985cbbe3bb4529de64eaad171119b2dd30b
Nekkunj <nekkunjpilani@gmail.com>
2025-04-19 15:29:28 -0400

    allowed hosts

--- server/config/settings.py
+++ server/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-hx5ebuq=yq5!ni@i4vl=6+_*^9zpp0#b+l)obhelzp!%ex%rl*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://event-planner-henon-f1a7c972e0e6.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost','https://event-planner-henon-f1a7c972e0e6.herokuapp.com/']
 
 
 # Application definition
