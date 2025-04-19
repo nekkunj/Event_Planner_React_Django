3a98f817289106783484f10ea5fdbae11e52a62d
Nekkunj <nekkunjpilani@gmail.com>
2025-04-19 19:08:32 -0400

    commit

--- config/settings.py
+++ config/settings.py
@@ -148,7 +148,7 @@ STATIC_URL = '/static/'
 # ]
 
 # Where to collect static files for production
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = BASE_DIR / 'staticfiles'  
 
 # Optional but recommended:
@@ -167,6 +167,6 @@ STORAGES = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# import dj_database_url
-# prod_db=dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+import dj_database_url
+prod_db=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
