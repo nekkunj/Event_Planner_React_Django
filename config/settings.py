e35c095cb2b5ea00fedc54abc5d4e7bb14c9e2ba
Nekkunj <nekkunjpilani@gmail.com>
2025-04-19 18:48:26 -0400

    changing whitelist

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-hx5ebuq=yq5!ni@i4vl=6+_*^9zpp0#b+l)obhelzp!%ex%rl*
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-CORS_ORIGIN_WHITELIST = ['localhost','http://event-planner-henon-f1a7c972e0e6.herokuapp.com','https://event-planner-henon-f1a7c972e0e6.herokuapp.com']
+CORS_ORIGIN_WHITELIST = ['http://localhost:8000','http://event-planner-henon-f1a7c972e0e6.herokuapp.com','https://event-planner-henon-f1a7c972e0e6.herokuapp.com']
 
 
 
