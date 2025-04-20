08c074da953653038bac9e947aa4ac39e5190479
Nekkunj <nekkunjpilani@gmail.com>
2025-04-19 21:53:04 -0400

    commit

--- config/settings.py
+++ config/settings.py
@@ -170,9 +170,9 @@ STORAGES = {
     },
 }
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
