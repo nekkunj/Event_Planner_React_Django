99770838ec61b67ce91c49119ac871cf0530c393
Nekkunj <nekkunjpilani@gmail.com>
2025-04-19 21:24:36 -0400

    changing templates

--- config/settings.py
+++ config/settings.py
@@ -76,7 +76,7 @@ WEBPACK_LOADER = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
